6b30d1bea1aa874e68d789488ea37e1cc6ab8b61
aminda007 <aminda.14@cse.mrt.ac.lk>
2018-09-19 16:51:18 +0530

    changed app name

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.static('./dist/@coreui/coreui-free-angular-admin-template'));
+app.use(express.static('./dist/rideme4u'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join('./dist/@coreui/coreui-free-angular-admin-template/index.html'));
+  res.sendFile(path.join('./dist/rideme4u/index.html'));
 });
 app.listen(process.env.PORT || 8080);
